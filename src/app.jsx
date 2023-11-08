9c6d83bdde218cbe3437c9859470a2627b7c8f24
Timothyvu99 <timothyvu99@gmail.com>
2023-11-07 19:12:17 -0800

    Complete

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import PocketBase from 'https://cdn.jsdelivr.net/npm/pocketbase@0.19.0/+esm'
+
+const USERNAME = 'timothyvu99@gmail.com'
+const PASSWORD = 'timothyvu99'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
+console.log(authData)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>Hello, {authData.admin.email}</h1>
+    </div>
+  )
+}
